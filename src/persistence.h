069d05b580292a39274a79a4957d6d60e0acc633
meerkqat <jurijslabanja@hotmail.com>
2016-06-04 19:18:51 +0200

    26 results on 100 pts

--- src/persistence.h
+++ src/persistence.h
@@ -43,7 +43,7 @@ private:
     double eps_ = -1;
     double eps_factor_ = 0.01;
     // do the calculation on the original dataset and this many shaken datasets
-    int num_shaken_datasets_ = 20;//100;
+    int num_shaken_datasets_ = 25;//100;
     // how many slices should [0,R] be divided into (delta param)
     int num_slices_ = 11;
 };
