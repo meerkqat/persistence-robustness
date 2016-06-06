e86b80293deb0fc5a8dcff0d637fe3f1bd6f64c6
meerkqat <jurijslabanja@hotmail.com>
2016-06-07 00:39:48 +0200

    diagram distance results

--- src/python/diagram_dist_calc.py
+++ src/python/diagram_dist_calc.py
@@ -8,19 +8,30 @@ fdiag = os.listdir("diagrams")
 
 diags = []
 
+noise = 1.3
+
 for fname in fdiag:
 	diags.append(PersistenceDiagram(2))
 	with open("diagrams/"+fname, "r") as f:
 		for line in f:
 			banana = line.split(" ")
-			pt = (float(banana[1]), float(banana[2]), int(banana[0]))
-			diags[-1].append(pt)
+			if len(banana) < 3:
+				continue
+			if float(banana[2])-float(banana[1]) > noise:
+				pt = (float(banana[1]), float(banana[2]), int(banana[0]))
+				diags[-1].append(pt)
+	print len(diags[-1])
 
 print "Calculating distances..."
 
+"""
+"""
 dists = []
 
 for i in range(len(diags)):
+	print i
 	dists.append(bottleneck_distance(diags[0], diags[i]))
 
 print dists
+"""
+"""
